28faf0b4a0c9aa1754493333ac334b4df1a9e47c
John Merz <jmerz826@gmail.com>
2021-11-03 17:05:57 -0400

    header done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 import "./App.css";
+import Header from "./components/header.js";
 
 function App() {
+
+  
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <Header />
     </div>
   );
 }
---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './header.css';
+
+const Header = (props) => {
+
+    return (
+        <div className="header-container">
+            <p>November 3, 2021</p>
+            <h1>🚀🚀 NASA Photo of the Day! 🚀🚀</h1>
+            <a href="https://www.nasa.gov" target="_blank">Nasa's Website!</a>
+        </div>
+    )
+}
+
+export default Header;
