81229ba99d1af7e5bd2e3b7661b53aa0de16aed9
John Merz <jmerz826@gmail.com>
2021-11-03 17:30:14 -0400

    Lot of progress. API being fetched properly, filling out content of site!

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import React, {useState, useEffect} from "react";
 import axios from "axios";
 import "./App.css";
 import Header from "./components/header.js";
+import { API_KEY } from './constants/index.js'
 
 function App() {
+  const [imageUrl, setImageUrl] = useState('');
+  const [imageTitle, setImageTitle] = useState('');
+  const [API_ENDPOINT, setAPI_ENDPOINT] = useState('');
 
+  useEffect(() => {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}${API_ENDPOINT}`)
+      .then(res => {
+        console.log(res.data);
+        setImageUrl(res.data.url);
+        setImageTitle(res.data.title)
+      })
+      .catch(err => console.error(err))
+  })
   
   return (
     <div className="App">
       <Header />
+      <div className="content-container">
+        <h2>{imageTitle}</h2>
+        <img src={imageUrl} alt={imageTitle} />
+      </div>
     </div>
   );
 }
