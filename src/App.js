789d73ba035db17ab5ed2f763af813ee6286cac0
John Merz <jmerz826@gmail.com>
2021-11-03 18:16:44 -0400

    started form, header now has live date

--- src/App.js
+++ src/App.js
@@ -8,23 +8,33 @@ function App() {
   const [imageUrl, setImageUrl] = useState('');
   const [imageTitle, setImageTitle] = useState('');
   const [API_ENDPOINT, setAPI_ENDPOINT] = useState('');
+  const [photoDate, setPhotoDate] = useState('');
 
   useEffect(() => {
     axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}${API_ENDPOINT}`)
       .then(res => {
         console.log(res.data);
         setImageUrl(res.data.url);
-        setImageTitle(res.data.title)
+        setImageTitle(res.data.title);
+        setPhotoDate(res.data.date);
       })
       .catch(err => console.error(err))
   })
   
-  return (
+  
+  return(
     <div className="App">
-      <Header />
+      <Header date={ photoDate}/>
       <div className="content-container">
         <h2>{imageTitle}</h2>
         <img src={imageUrl} alt={imageTitle} />
+        <div className="date-selector">
+          <form action="/action_page.php">
+            Month (MM): <input type="text" name="month" />
+            Day (DD): <input type="text" name="day" />
+
+          </form>
+        </div>
       </div>
     </div>
   );
--- src/components/header.js
+++ src/components/header.js
@@ -2,10 +2,9 @@ import React from "react";
 import './header.css';
 
 const Header = (props) => {
-
     return (
         <div className="header-container">
-            <p>November 3, 2021</p>
+            <p>{props.date}</p>
             <h1>🚀🚀 NASA Photo of the Day! 🚀🚀</h1>
             <a href="https://www.nasa.gov" target="_blank">Nasa's Website!</a>
         </div>
